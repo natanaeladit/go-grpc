94a80611424b5c58c49dcccb26a03a1eac4fb47d
natanaeladit <nata.adit@gmail.com>
2021-06-27 22:46:17 +0800

    update service

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 	"log"
 	"net"
 
-	"github.com/natanaeladit/go-grpc/chat"
+	"github.com/natanaeladit/go-grpc/chat_service"
+
 	"google.golang.org/grpc"
 )
 
@@ -18,11 +19,11 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	s := chat.Server{}
+	s := chat_service.Server{}
 
 	grpcServer := grpc.NewServer()
 
-	chat.RegisterChatServiceServer(grpcServer, &s)
+	chat_service.RegisterChatServiceServer(grpcServer, &s)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %s", err)
